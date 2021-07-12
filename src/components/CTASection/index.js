c2891ba50ff8fa3c3b6b4077b93c0337fe856075
Eugene Martynenko <eugene.martynenko@dataart.com>
2021-07-12 19:19:50 +0300

    anchor updated

--- src/components/CTASection/index.js
+++ src/components/CTASection/index.js
@@ -13,7 +13,7 @@ const CTASection = ({ title, description }) => {
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <a
-              href="#concact"
+              href="#contact"
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
             >
               Замовити
